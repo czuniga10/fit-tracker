36fd061d7dc8e5ae8b386db6e1fac4ea6a453992
Chad <chad.zuniga10@gmail.com>
2018-06-08 22:24:10 -0600

    created first router for user table

---
+++ server/routers/delegate.routers.js
@@ -0,0 +1,13 @@
+const userRouter = require('./user.router')
+
+
+function delegateRoutes(app) {
+    app.use('/api/user', userRouter);
+    
+    app.all('*', (req, res) => {
+        res.status(404).send({message: "Cannot access any resources at " + req.originalUrl });
+    });    
+    
+    return app;
+}
+module.exports = delegateRoutes;
