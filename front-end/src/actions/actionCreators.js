d7962853c298fe3f0ab31d482e1ef28ee7a0dc07
Chad <chad.zuniga10@gmail.com>
2018-06-29 23:55:08 -0600

    deleted unneeded functions in actionCreator

--- front-end/src/actions/actionCreators.js
+++ front-end/src/actions/actionCreators.js
@@ -2,10 +2,7 @@
 export const UPDATE_USER = "UPDATE_USER";
 export const UPDATE_PROJECT = "UPDATE_PROJECT";
 export const UPDATE_LIFT = "UPDATE_LIFT";
-// export const ADD_TO_CART = "ADD_TO_CART";
-// export const GET_CARD = "GET_CARD";
-// export const EMPTY_CART = "EMPTY_CART";
-// export const DELETE_ITEM = "DELETE_ITEM";
+
 
 // export function updateAuth(boolean){
 //     return {
@@ -35,28 +32,3 @@ export function updateLift(lift){
     }
 }
 
-// export function addToCart(product){
-//     return {
-//         type: ADD_TO_CART,
-//         payload: product
-//     }
-// }
-
-// export function emptyCart(){
-//     return {
-//         type: EMPTY_CART,
-//     }
-// }
-
-// export function deleteItem() {
-//     return {
-//         type: DELETE_ITEM
-//     }
-// }
-
-// export function getCard(card){
-//     return {
-//         type: GET_CARD,
-//         payload: card
-//     }
-// }
